d30e2d084a34a2ed5aa8ad9dfd24e4d6b9e71a8b
arturhbs <arturhenrique71@gmail.com>
2020-10-07 19:10:59 -0300

    Fixing graphics to match the 5 publishers

--- subscriber/sub.py
+++ subscriber/sub.py
@@ -10,13 +10,11 @@ import seaborn as sns
 
 
 
-metricAvg = {'cpuTimeAvg_0':[],'cpuTimeAvg_1':[],'cpuTimeAvg_2':[],'cpuTimeAvg_3':[],'cpuTimeAvg_4':[],
-            'cpuTimePIDAvg_0':[] ,'cpuTimePIDAvg_1':[],'cpuTimePIDAvg_2':[],'cpuTimePIDAvg_3':[],'cpuTimePIDAvg_4':[],
-            'memVirtualAvg_0':[],'memVirtualAvg_1':[],'memVirtualAvg_2':[],'memVirtualAvg_3':[],'memVirtualAvg_4':[] ,
-            'memInfoAvg_0':[],'memInfoAvg_1':[],'memInfoAvg_2':[],'memInfoAvg_3':[],'memInfoAvg_4':[],
-            'diskUsageAvg_0':[],'diskUsageAvg_1':[],'diskUsageAvg_2':[],'diskUsageAvg_3':[],'diskUsageAvg_4':[] }
-
-finished = 0
+metricAvg = {'cpuTimeAvg_ax0':[],'cpuTimeAvg_ax1':[],'cpuTimeAvg_ax2':[],'cpuTimeAvg_ax3':[],'cpuTimeAvg_ax4':[],
+            'cpuTimePIDAvg_ax0':[] ,'cpuTimePIDAvg_ax1':[],'cpuTimePIDAvg_ax2':[],'cpuTimePIDAvg_ax3':[],'cpuTimePIDAvg_ax4':[],
+            'memVirtualAvg_ax0':[],'memVirtualAvg_ax1':[],'memVirtualAvg_ax2':[],'memVirtualAvg_ax3':[],'memVirtualAvg_ax4':[] ,
+            'memInfoAvg_ax0':[],'memInfoAvg_ax1':[],'memInfoAvg_ax2':[],'memInfoAvg_ax3':[],'memInfoAvg_ax4':[],
+            'diskUsageAvg_ax0':[],'diskUsageAvg_ax1':[],'diskUsageAvg_ax2':[],'diskUsageAvg_ax3':[],'diskUsageAvg_ax4':[] }
 
 
 def on_connect(client, userdata, flags, rc):
@@ -24,7 +22,7 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and reconnect then subscriptions will be renewed.
     # client.subscribe("$SYS/#") # Show every message
-    client.subscribe([('finished',0),('cpuTimeAvg',0),('cpuTimePIDAvg',0),('memVirtualAvg',0),('memInfoAvg',0),('diskUsageAvg',0)],qos=1)
+    client.subscribe([('cpuTimeAvg',0),('cpuTimePIDAvg',0),('memVirtualAvg',0),('memInfoAvg',0),('diskUsageAvg',0)],qos=1)
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
@@ -37,109 +35,101 @@ def on_message(client, userdata, msg):
 
 # Identify message recieved
 def read_message(topic,msgMetric,msgIndex):
-    print('topic = ', topic)
-    print('msg index', msgIndex)
+    # print('topic = ', topic)
+    # print('msg index', msgIndex)
     if topic == 'cpuTimeAvg':
         # Get metric for 8 loops
         if msgIndex == 0:
-            metricAvg['cpuTimeAvg_0'].append(msgMetric)
+            metricAvg['cpuTimeAvg_ax0'].append(msgMetric)
             
         # Get metric for 13 loops
         elif msgIndex == 1:   
-            metricAvg['cpuTimeAvg_1'].append(msgMetric)
+            metricAvg['cpuTimeAvg_ax1'].append(msgMetric)
 
         # Get metric for 21 loops
         elif msgIndex == 2:   
-            metricAvg['cpuTimeAvg_2'].append(msgMetric)
+            metricAvg['cpuTimeAvg_ax2'].append(msgMetric)
             
         # Get metric for 34 loops
         elif msgIndex == 3:   
-            metricAvg['cpuTimeAvg_3'].append(msgMetric)
+            metricAvg['cpuTimeAvg_ax3'].append(msgMetric)
             
         # Get metric for 55 loops
         elif msgIndex == 4:   
-            metricAvg['cpuTimeAvg_4'].append(msgMetric)
+            metricAvg['cpuTimeAvg_ax4'].append(msgMetric)
             
     elif topic == 'cpuTimePIDAvg':
         # Get metric for 8 loops
         if msgIndex == 0:
-            metricAvg['cpuTimePIDAvg_0'].append(msgMetric)
+            metricAvg['cpuTimePIDAvg_ax0'].append(msgMetric)
             
         # Get metric for 13 loops
         elif msgIndex == 1:   
-            metricAvg['cpuTimePIDAvg_1'].append(msgMetric)
+            metricAvg['cpuTimePIDAvg_ax1'].append(msgMetric)
 
         # Get metric for 21 loops
         elif msgIndex == 2:   
-            metricAvg['cpuTimePIDAvg_2'].append(msgMetric)
+            metricAvg['cpuTimePIDAvg_ax2'].append(msgMetric)
             
         # Get metric for 34 loops
         elif msgIndex == 3:   
-            metricAvg['cpuTimePIDAvg_3'].append(msgMetric)
+            metricAvg['cpuTimePIDAvg_ax3'].append(msgMetric)
 
         # Get metric for 55 loops
         elif msgIndex == 4:   
-            metricAvg['cpuTimePIDAvg_4'].append(msgMetric)
+            metricAvg['cpuTimePIDAvg_ax4'].append(msgMetric)
             
     elif topic == 'memVirtualAvg':
         if msgIndex == 0:
-            metricAvg['memVirtualAvg_0'].append(msgMetric)
+            metricAvg['memVirtualAvg_ax0'].append(msgMetric)
             
         elif msgIndex == 1:   
-            metricAvg['memVirtualAvg_1'].append(msgMetric)
+            metricAvg['memVirtualAvg_ax1'].append(msgMetric)
             
         elif msgIndex == 2:   
-            metricAvg['memVirtualAvg_2'].append(msgMetric)
+            metricAvg['memVirtualAvg_ax2'].append(msgMetric)
             
         elif msgIndex == 3:   
-            metricAvg['memVirtualAvg_3'].append(msgMetric)
+            metricAvg['memVirtualAvg_ax3'].append(msgMetric)
             
         elif msgIndex == 4:   
-            metricAvg['memVirtualAvg_4'].append(msgMetric)
+            metricAvg['memVirtualAvg_ax4'].append(msgMetric)
             
     elif topic == 'memInfoAvg':
         if msgIndex == 0:
-            metricAvg['memInfoAvg_0'].append(msgMetric)
+            metricAvg['memInfoAvg_ax0'].append(msgMetric)
             
         elif msgIndex == 1:   
-            metricAvg['memInfoAvg_1'].append(msgMetric)
+            metricAvg['memInfoAvg_ax1'].append(msgMetric)
             
         elif msgIndex == 2:   
-            metricAvg['memInfoAvg_2'].append(msgMetric)
+            metricAvg['memInfoAvg_ax2'].append(msgMetric)
             
         elif msgIndex == 3:   
-            metricAvg['memInfoAvg_3'].append(msgMetric)
+            metricAvg['memInfoAvg_ax3'].append(msgMetric)
             
         elif msgIndex == 4:   
-            metricAvg['memInfoAvg_4'].append(msgMetric)
+            metricAvg['memInfoAvg_ax4'].append(msgMetric)
             
     elif topic == 'diskUsageAvg':
         if msgIndex == 0:
-            metricAvg['diskUsageAvg_0'].append(msgMetric)
+            metricAvg['diskUsageAvg_ax0'].append(msgMetric)
             
         elif msgIndex == 1:   
-            metricAvg['diskUsageAvg_1'].append(msgMetric)
+            metricAvg['diskUsageAvg_ax1'].append(msgMetric)
             
         elif msgIndex == 2:   
-            metricAvg['diskUsageAvg_2'].append(msgMetric)
+            metricAvg['diskUsageAvg_ax2'].append(msgMetric)
             
         elif msgIndex == 3:   
-            metricAvg['diskUsageAvg_3'].append(msgMetric)
+            metricAvg['diskUsageAvg_ax3'].append(msgMetric)
             
         elif msgIndex == 4:   
-            metricAvg['diskUsageAvg_4'].append(msgMetric)
-    #
-    # print("finished = ", finished)
-    # if topic == 'finished':
-    #     finished += 1
-    print("\n ************* Metric : \n", metricAvg)
-    # print('LEN == ',len(metricAvg['diskUsageAvg_3']))
-    if(len(metricAvg['diskUsageAvg_3']) == 3 ):
+            metricAvg['diskUsageAvg_ax4'].append(msgMetric)
+   
+    if(len(metricAvg['diskUsageAvg_ax4']) == 5 ):
         create_graph()
-    # if nameMetricIndex ==  'diskUsageAvg_4':
-    #     print("tamanho disk4: ",len(metric))
-    # if len(metric) == 5 :
-    # create_graph()
+   
 
 def line_chart(X,Y, nameImage):
     plt.clf()
@@ -153,32 +143,29 @@ def line_chart(X,Y, nameImage):
     plt.clf()
 
 def transform_data_for_graph():
+    print(metricAvg)
     cpuTimeArray = []
     cpuTimePIDArray = []
     memVirtualArray = []
     memInfoArray = []
     diskUsageArray = []
 
-    print("metricAVG = ")
-    print(metricAvg)
     for metric in metricAvg :
         if len(metricAvg[metric]) != 0 :
             metricAvg[metric] = statistics.mean(metricAvg[metric])
                     
-    print(metricAvg)
     fibonacci = ['8','13','21','34','55']
-    for i in range(3):
-        j = str(i)
-        cpuTimeArray.append(metricAvg['cpuTimeAvg_'+j])
-        cpuTimePIDArray.append(metricAvg['cpuTimePIDAvg_'+j])
-        memVirtualArray.append(metricAvg['memVirtualAvg_'+j])
-        memInfoArray.append(metricAvg['memInfoAvg_'+j])
-        diskUsageArray.append(metricAvg['diskUsageAvg_'+j])
+    for i in range(5):
+        i = str(i)
+        cpuTimeArray.append(metricAvg['cpuTimeAvg_ax'+i])
+        cpuTimePIDArray.append(metricAvg['cpuTimePIDAvg_ax'+i])
+        memVirtualArray.append(metricAvg['memVirtualAvg_ax'+i])
+        memInfoArray.append(metricAvg['memInfoAvg_ax'+i])
+        diskUsageArray.append(metricAvg['diskUsageAvg_ax'+i])
 
     return fibonacci,cpuTimeArray,cpuTimePIDArray,memInfoArray,memVirtualArray,diskUsageArray
     
 def create_graph():
-    print('*********************')
     fibonacci,cpuTimeArray,cpuTimePIDArray,memInfoArray,memVirtualArray,diskUsageArray = transform_data_for_graph()
     
     line_chart(fibonacci, cpuTimeArray, 'cpuTime')
